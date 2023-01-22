c8aeb014bf79bb87217643857b9575f2656d9b86
isOdin <44323275+Proshnick2016@users.noreply.github.com>
2023-01-22 17:46:12 +0000

    3weeksBeforeDeadLine

--- backend/src/controller/logInDataParse.ts
+++ backend/src/controller/logInDataParse.ts
@@ -9,16 +9,8 @@ export async function postHttp (url, body) {
     axios.post(url, body)
       .then(function (response) {
         const cookieForLogIn = response.headers['set-cookie'];
-        
+
         resolve(cookieForLogIn);
-        // HTTP GET ЗАПРОС для созадния массива из информации об уроках (ПОТОМ: отправки в базу данных json переменную с информацией об уроках)
-        /* parseSchoolWeekPage('https://edu.gounn.ru/journal-app/week.-1', // url для СЛЕДУЮЩИЕЙ НЕДЕЛИ
-          {
-            optionLessonNumber: '.dnevnik-lesson__number', // параметр для номера урока
-            optionLessonName: '.js-rt_licey-dnevnik-subject', // параметр для названия урока
-            optionLessonTime: '.dnevnik-lesson__time' // параметр для времени урока
-          },
-          cookieForLogIn); // полученные 'cookie' */
       })
       .catch(function (error) { // если получили ошибку
         console.log(error);
@@ -28,18 +20,18 @@ export async function postHttp (url, body) {
 
 // HTTP GET FUNCTION
 export async function parseSchoolWeekPage (url, cookieForLogIn) {
-  return new Promise(function(resolve, reject){
+  return new Promise(function (resolve, reject) {
     axios.get(url, { // получение html странички
-        headers: {
-          Cookie: cookieForLogIn
-        }
-    }).then(function(response){
+      headers: {
+        Cookie: cookieForLogIn
+      }
+    }).then(function (response) {
       const processedPage = cheerio.load(response.data); // передаём обработанную старничку в одну переменную 'processedPage'
-      console.log(processedPage)
       resolve(processedPage);
+    }).catch(function(error){
+      console.log(error)
     })
-  })
-  // formLessonsInfo(processedPage, optionsToGetElement); // создания массива из информации об уроках
+  });
 };
 
 export function formLessonsInfo (processedPage, optionsToGetElement) {
@@ -48,9 +40,9 @@ export function formLessonsInfo (processedPage, optionsToGetElement) {
   const allLessonsInfoWithWeekDays = { Понедельник: {}, Вторник: {}, Среда: {}, Четверг: {}, Пятница: {}, Суббота: {} }; // массив для распределения уроков по дням
   let dayIndexNum = 0; // Индекс дня в объекте
 
-  const lessonNumber = getArrayByOptionElement(processedPage, optionsToGetElement.optionLessonNumber).then(function (result) { return result; }); // получаем массив данных для числа
-  const lessonsNames = getArrayByOptionElement(processedPage, optionsToGetElement.optionLessonName).then(function (result) { return result; }); // получаем массив данных для имени
-  const lessonTime = getArrayByOptionElement(processedPage, optionsToGetElement.optionLessonTime).then(function (result) { return result; }); // получаем массив данных для времени
+  const lessonNumber = getArrayByOptionElement(processedPage, optionsToGetElement.optionLessonNumber); // получаем массив данных для числа
+  const lessonsNames = getArrayByOptionElement(processedPage, optionsToGetElement.optionLessonName); // получаем массив данных для имени
+  const lessonTime = getArrayByOptionElement(processedPage, optionsToGetElement.optionLessonTime); // получаем массив данных для времени
 
   for (let i = 0; i < lessonsNames.length; i++) { // создаём объект, взяв одинаковые записи у всех массивов, и передаём его в массив. Получаем массив с объектами.
     if (Number(lessonNumber[i]) !== 0) {
@@ -67,23 +59,23 @@ export function formLessonsInfo (processedPage, optionsToGetElement) {
       dayIndexNum++;
     }
   }
-  // console.log(allLessonsInfoWithWeekDays);
-  return (allLessonsInfoWithWeekDays);
+
+  return allLessonsInfoWithWeekDays;
 }
 
-async function makeNewLessonObj (lessonNumber, lessonName, lessonTime) { // создаём объект для одного предмета
+function makeNewLessonObj (lessonNumber, lessonName, lessonTime) { // создаём объект для одного предмета
   lessonNumber = lessonNumber.trim(); // мы получаем неправильную строку, например, '\n       8', поэтому убираем всё лишнее, оставляя только число
 
-  return ({
+  return {
     lessonNumber,
     lessonName,
     lessonTime
-  });
+  };
 }
 
 function getArrayByOptionElement (processedPage, optionsToGetElementToArray) { // создаём и выводим массив данных на основе предоставленных параметров(номер/имя/время)
-  return (processedPage(optionsToGetElementToArray).map(
+  return processedPage(optionsToGetElementToArray).map(
     function () {
       return processedPage(this).text();
-    }).toArray());
+    }).toArray();
 }
--- backend/src/controller/user-controller.ts
+++ backend/src/controller/user-controller.ts
@@ -1,19 +1,19 @@
 import { Body, Controller, OnUndefined, Post } from 'routing-controllers';
 import 'reflect-metadata';
 import { loginModel } from '../model/info';
-import { postHttp, parseSchoolWeekPage } from './logInDataParse';
+import { postHttp, parseSchoolWeekPage, formLessonsInfo } from './logInDataParse';
 
 @Controller()
 export class UserController {
   @Post('/userLogIn')
   @OnUndefined(204)
   async postOne (@Body() logInFormData: loginModel) {
-    /* const optionsToGetElement =
+    const optionsToGetElement =
     {
       optionLessonNumber: '.dnevnik-lesson__number', // параметр для номера урока
       optionLessonName: '.js-rt_licey-dnevnik-subject', // параметр для названия урока
       optionLessonTime: '.dnevnik-lesson__time' // параметр для времени урока
-    }; */
+    };
 
     // HTTP POST ЗАПРОС на сайт школы, для получения 'cookie'
     postHttp('https://edu.gounn.ru/ajaxauthorize', {
@@ -21,13 +21,14 @@ export class UserController {
       username: logInFormData.login,
       password: logInFormData.password
     })
-      .then(function (result) { // получение processedPage
-        console.log(result);
-        parseSchoolWeekPage('https://edu.gounn.ru/journal-app/week.-1', result) // url для СЛЕДУЮЩИЕЙ НЕДЕЛИ + полученные 'cookie'
-      }).then(function (result){
-        console.log(2)
-        console.log(result)
+      .then(function (cookieForLogIn) { // получение processedPage
+        // HTTP GET ЗАПРОС для созадния массива из информации об уроках
+        parseSchoolWeekPage('https://edu.gounn.ru/journal-app/week.-1', cookieForLogIn)// url для СЛЕДУЮЩИЕЙ НЕДЕЛИ + полученные 'cookie'
+          .then(function (processedPage) { 
+            // получение школьного расписания
+            const recievedSchoolSchedual = formLessonsInfo(processedPage, optionsToGetElement);
+            console.log(recievedSchoolSchedual);
+        });
       });
-    // return recievedSchoolSchedual;
   }
 }
