5a1bb39f2e0105a7af244dedf2edabd9eafed02b
isOdin <44323275+Proshnick2016@users.noreply.github.com>
2023-01-29 13:13:40 +0000

    2weeksBeforeDeadLine

--- client/App.js
+++ client/App.js
@@ -5,25 +5,9 @@ import HomePage from "./pages/HomePage";
 import Schedual from "./pages/Schedual";
 
 export default function App() {
-  const [items, setItems] = React.useState();
-  const [getten, setStatus] = React.useState(false);
-
-  React.useEffect(() => {
-    if (getten === false) {
-      const schedualFromLocaStorage = JSON.parse(localStorage.getItem('Schedual'))
-      setItems(schedualFromLocaStorage);
-      setStatus(true);
-      console.log(schedualFromLocaStorage['Понедельник'][0])
-    }
-  });
-
   return (
     <View>
-      <FlatList data={items} renderItem={({ item }) => <Schedual
-        lessonNumber={item['Понедельник'][0].lessonNumber}
-        lessonName={item['Понедельник'][0].lessonName}
-        lessonTime={item['Понедельник'][0].lessonTime} />} />
-
+      <HomePage />
       <StatusBar theme="auto" />
     </View>
   );
